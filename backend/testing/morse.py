24c0c09f00248ceba4f6b0b051ab5e1ce8ae7438
LagaeJens <jens.lagae@student.howest.be>
2022-06-01 11:18:47 +0200

    uitlezen & schrijven lukt

--- backend/testing/morse.py
+++ backend/testing/morse.py
@@ -3,14 +3,26 @@ from time import sleep
 from smbus import SMBus
 from random import randint
 
-i2c = SMBus(1)
 
+
+i2c = SMBus(1)
 def random_nummer_genereren():
-    random_nummer = randint(1, 9)
+    random_nummer = randint(1,9)
     return random_nummer
 
-def flash_led():
-    x = (test)*2
+
+def flash_led_2(nummer):
+    x = (nummer)*2
+    # print(x)
+    for i in range(x):
+        # print("aan")
+        waardes = i2c.read_byte(0x24)
+        cijfers = waardes ^ 0x20
+        i2c.write_byte(0x24, cijfers) 
+        sleep(0.5)
+        
+def flash_led_1(nummer):
+    x = (nummer)*2
     # print(x)
     for i in range(x):
         # print("aan")
@@ -19,64 +31,55 @@ def flash_led():
         i2c.write_byte(0x24, cijfers) 
         sleep(0.5)
 
-try:
-    test = random_nummer_genereren()
-    print(test)
-    while True:
-        i2c.open(1)
+def bcd_uitlezen():
         waarde = i2c.read_byte(0x24)
         cijfer = (waarde & 0x0F) ^ 0x0F 
         print(cijfer)
+        print(bin(cijfer))    
+        return cijfer
+
+
+f = False
+e = False
+
+try:
+    nummer_1 = random_nummer_genereren()
+    nummer_2 = random_nummer_genereren()
+    print(nummer_1)
+    print(nummer_2)
+    i2c.open(1)
+    
+          
+    while True:
+        if f == False:
+            flash_led_1(nummer_1)
+        if e == True:
+            flash_led_2(nummer_2)
+        sleep(0.001)
+        i2c.write_byte(0x24, 255)
+        sleep(0.001)
+        # i2c.write_byte(0x24, 0)
+        # waarde = i2c.read_byte(0x24)
+        # cijfer = (waarde & 0x0F) ^ 0x0F 
+        # print(cijfer)
+        cijfer = bcd_uitlezen()
         # print(bin(cijfer))
         # print(bin(waarde))
-        flash_led()
-        i2c.close()
-        
-    
+        if cijfer == nummer_1:
+            print("nr1 OK")
+            f = True
+            e = True
+        if cijfer == nummer_2:
+            print("nr2 OK")
+            e = False
+            break
+        sleep(1)
+
+
 
 except KeyboardInterrupt as e:
     print(e)
-
+    
 finally:
     GPIO.cleanup()
     i2c.close()
-# GPIO.setmode(GPIO.BCM)
-
-# print("Script is running!!!")
-
-# PinB1 = 23
-# PinB2 = 24
-# PinB3 = 25
-# PinB4 = 26
-
-# GPIO.setup(PinB1,GPIO.IN,pull_up_down=GPIO.PUD_UP)
-# GPIO.setup(PinB2,GPIO.IN,pull_up_down=GPIO.PUD_UP)
-# GPIO.setup(PinB3,GPIO.IN,pull_up_down=GPIO.PUD_UP)
-# GPIO.setup(PinB4,GPIO.IN,pull_up_down=GPIO.PUD_UP)
-
-# try:
-#     while True:
-
-        # waarde1 = not GPIO.input(PinB1) 
-        # waarde2 = not GPIO.input(PinB2)
-        # waarde3 = not GPIO.input(PinB3)
-        # waarde4 = not GPIO.input(PinB4)
-        # print(waarde1,waarde2,waarde3,waarde4)
-        
-#         result = waarde1  
-#         result = result | (waarde2<<1)
-#         result = result | (waarde3<<2)
-#         result |= waarde4<<3
-#         print(result)
-
-#         # result = waarde1 | (waarde2 << 1) | (waarde3 << 2) | (waarde4 << 3)
-#         # print(result)
-
-
-# except KeyboardInterrupt as e:
-#     print(e)
-#     #pass
-# finally:
-#     GPIO.cleanup()
-#     print("Script has stopped!!!")
-
