91a9eb6f8ca2dcc8909f08c09fb62dce37aa7c1b
LagaeJens <jens.lagae@student.howest.be>
2022-06-10 20:35:45 +0200

    puzzelgame klaar

--- backend/app.py
+++ backend/app.py
@@ -217,18 +217,14 @@ def lcd_ip():
         #display leegmaken 
         lcd.send_instruction(0x01)
         time.sleep(0.05)
+        lcd.wifi_adress()
+        
         while True:
-            i2c.open(1)
-            # i2c.write_byte(0x20, 0x00)
-            # servo.main_servo_met_mpu()
-            lcd.send_instruction(0x01) #display leegmaken
-            lcd.ip_adress_ophalen() 
-            lcd.send_instruction(0x0C)
-            # dungeon.dungeons_main()
-            # test.main_BCD()
             time.sleep(1)
     except Exception as e:
         print(e)
+        
+        
 # def rfid_thread(id):
 #     try:
 #         while True:
