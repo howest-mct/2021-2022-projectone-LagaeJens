c00b96e825d6c74d66d1031382921e0033dc4245
LagaeJens <jens.lagae@student.howest.be>
2022-06-11 22:06:37 +0200

    commit

--- backend/testing/Classen_test_program.py
+++ backend/testing/Classen_test_program.py
@@ -29,6 +29,7 @@ test=BCD()
 dungeon = dungeons()
 try:
     setup()
+    var_a = False
     # lcd.setup_lcd()
     #     #function set
     # lcd.send_instruction(0x38)
@@ -41,16 +42,22 @@ try:
     # sleep(0.05)
     # # test.setup()
     while True:
-        i2c.open(1)
+        if var_a == False:
+            i2c.open(1)
         # i2c.write_byte(0x20, 0x00)
-        servo.main_servo_met_mpu()
+            waarde=servo.main_servo_met_mpu()
+            if waarde == 1:
+                print("1")
+                var_a = True
+        
+        
         # lcd.send_instruction(0x01) #display leegmaken
         # lcd.ip_adress_ophalen() 
         # lcd.send_instruction(0x0C)
         # dungeon.dungeons_main()
         # test.main_BCD()
         sleep(1)
-        
+    
         
         
         
